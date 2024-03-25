8a2306017188f5cf60562e531a1238788ccafdcb
Nehakumari-s <kumarineha98071@gmail.com>
2024-03-25 23:52:14 +0530

    first commit

--- vaccine.c
+++ vaccine.c
@@ -1,26 +1,82 @@
 
 // VACCINE  REGISTRATION VACCINATION 
 #include<stdio.h>
+#include<string.h>
+void display_user();
+void register_user();
+
+int maxRegistrations = 100;
+int nameLength = 50;
+int registrationIds[100];
+char names[100][50];
+int ages[100];
+int numRegistrations = 0;
+
+
+//function to register the user
+
+void register_user()
+{
+	if(numRegistrations == maxRegistrations)
+	{
+		printf("registration limit reached.\n");
+	}
+	 int id;
+	 int i;
+	 printf("enter id:");
+	 scanf("%d",&id);
+	 
+	 for (i = 0; i < numRegistrations; i++) {
+        if (registrationIds[i] == id) {
+            printf("ID already exists. Please enter a different ID.\n");
+            return;
+        }
+    }
+
+    registrationIds[numRegistrations] = id;
+    printf("Enter Name: ");
+    scanf("%s", names[numRegistrations]);
+    printf("Enter Age: ");
+    scanf("%d", &ages[numRegistrations]);
+
+    numRegistrations++;
+    printf("Registration successful.\n");
+}
+
+
+//function to display user
+void display_users()
+{
+	
+}
+
+
+
 int main() 
  {
-    char name[100];
-    int age;
-    char address[100];
-    char vaccineType[100];
-    printf("Please provide the following information:\n");
-    printf("Name: ");   //user provides their name
-    scanf("%s", name);
-    printf("Age: ");    //user provides their age 
-    scanf("%d", &age);
-    printf("Address: ");
-    scanf("%s", address);
-    printf("Vaccine Type: ");
-    scanf("%s", vaccineType);
-    printf("\nThank you for registering!\n");
-    printf("Your registration details:\n");
-    printf("Name: %s\n", name);
-    printf("Age: %d\n", age);
-    printf("Address: %s\n", address);
-    printf("Vaccine Type: %s\n", vaccineType);
-return 0;
+    int choice;
+    do {
+        printf("\nVaccine Registration Management\n");
+        printf("1. Register User\n");
+        printf("2. Display Registered Users\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                register_user();
+                break;
+            case 2:
+                display_users();
+                break;
+            case 3:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 3);
+
+
 }
